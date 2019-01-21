b04a160edfd2e7858c73864fec4cd011c2eb39e6
Timur Tagaev <dead.file.000@gmail.com>
2019-01-21 18:24:23 +0200

    created 'first project' module

---
+++ firstSeleniumProject/src/test/java/OpenChromeBrowser.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class OpenChromeBrowser {
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp()
+    {
+        wd=new ChromeDriver();
+    }
+
+    @Test
+    public void chromeBrowserTest(){
+        wd.get("https:/www.google.com");
+    }
+
+    @AfterClass
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+      wd.quit();
+    }
+}
